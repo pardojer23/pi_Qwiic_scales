545e7fd2e95cf65603d186e0a5eb4bad2edd39e4
pardojer23 <mezeg39@gmail.com>
2021-03-30 08:53:07 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(name='pi_Qwiic_scales',
+      version='0.01',
+      description='Raspberry pi Qwiic scales',
+      author='Jeremy Pardo',
+      author_email='mezeg39@gmail.com',
+      packages=find_packages(),
+      install_requires=[
+                'PyNAU7802',
+                'smbus2',
+                'qwiic'])
