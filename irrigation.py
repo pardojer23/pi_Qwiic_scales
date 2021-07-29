da36ed308f5b0cb7582ab54699c66185e05e8eac
pardojer23 <mezeg39@gmail.com>
2021-07-29 14:17:32 -0400

    testing solenoid control

--- irrigation.py
+++ irrigation.py
@@ -4,29 +4,41 @@ import busio
 import time
 i2c = busio.I2C(board.SCL, board.SDA)
 import RPi.GPIO as GPIO
-#Assign channels to variables to keep track of them
-s1 = 13
-s2 = 16
-s3 = 19
-s4 = 20
-s5 = 26
-s6 = 21
+import sync_data
+
 
 # Set GPIO pins to use BCM pin numbers
 GPIO.setmode(GPIO.BCM)
 
 # Set digital pin 24 to an input
 GPIO.setup(24, GPIO.IN)
-# Set solenoid driver pins to outputs:
-GPIO.setup(s1, GPIO.OUT)  # set Solenoid 1 output
-GPIO.setup(s2, GPIO.OUT)  # set Solenoid 2 output
-GPIO.setup(s3, GPIO.OUT)  # set Solenoid 3 output
-GPIO.setup(s4, GPIO.OUT)  # set Solenoid 4 output
-GPIO.setup(s5, GPIO.OUT)  # set Solenoid 5 output
-GPIO.setup(s6, GPIO.OUT)  # set Solenoid 6 output
 
-#Test Solenoids by turning each on for a half second
-GPIO.output(s1, GPIO.HIGH) #turn solenoid 1 on
-time.sleep(0.5) #Wait for a half second
-GPIO.output(s1, GPIO.LOW) #turn solenoid 1 off
-time.sleep(0.5)
+
+class Solenoid:
+    def __init__(self, channel):
+        self.channel = channel
+        GPIO.setup(channel, GPIO.OUT)
+
+    def open_valve(self):
+        GPIO.output(self.channel, GPIO.HIGH)
+
+    def close_valve(self):
+        GPIO.output(self.channel, GPIO.LOW)
+
+    def water(self, amount, rate=2.103):
+        open_time = float(amount) / float(rate)
+        print("watering for {0} minutes".format(round(open_time/60, ndigits=2)))
+        self.open_valve()
+        time.sleep(open_time)
+        self.close_valve()
+
+def main():
+    s1 = Solenoid(13)
+    s1.water(amount=10)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
